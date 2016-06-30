bea511c662d1eb82a8a3189720e60d23d5510e92
Niels Kjer <nielskjer@gmail.com>
2016-06-30 15:32:08 -0400

    Isolated graphing per company basis
    Implements binary search in order to find a given company in the list of  all companies

--- src/main/java/com/vogella/maven/quickstart/GraphViewer.java
+++ src/main/java/com/vogella/maven/quickstart/GraphViewer.java
@@ -2,6 +2,8 @@ package com.vogella.maven.quickstart;
 
 import edu.uci.ics.jung.algorithms.layout.CircleLayout;
 import edu.uci.ics.jung.algorithms.layout.Layout;
+import edu.uci.ics.jung.algorithms.shortestpath.DijkstraShortestPath;
+import edu.uci.ics.jung.algorithms.shortestpath.DistanceStatistics;
 import edu.uci.ics.jung.graph.DirectedGraph;
 import edu.uci.ics.jung.graph.DirectedSparseGraph;
 import edu.uci.ics.jung.graph.DirectedSparseMultigraph;
@@ -9,6 +11,7 @@ import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.SparseGraph;
 import edu.uci.ics.jung.graph.util.EdgeType;
 import edu.uci.ics.jung.visualization.BasicVisualizationServer;
+import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.control.DefaultModalGraphMouse;
 import edu.uci.ics.jung.visualization.control.ModalGraphMouse;
 import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
@@ -19,8 +22,13 @@ import java.awt.Dimension;
 import java.awt.Paint;
 import java.awt.Stroke;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Queue;
 import java.util.Random;
+import java.util.Scanner;
+
+import org.apache.commons.lang3.StringUtils;
 
 import javax.swing.JFrame;
 import org.apache.commons.collections15.Transformer;
@@ -28,33 +36,63 @@ import org.apache.commons.collections15.Transformer;
 public class GraphViewer {
 	
 	//Create Graph
-	Graph<String, String> g; 
+	private static Graph<String, String> g; 
+	private ArrayList<String> tmpNames = new ArrayList<String>();
+	private ArrayList<Float> prb = new ArrayList<Float>();
+	private static Scanner scanner = new Scanner(System.in);
 	
-	//Constructor
+	/** Helper Functions **/ 
 	
+	//Sort String Array by alphabetical order while respectively reordering the probability array
+
+	//Uses binary search in order to find the starting location of a company in an arraylist
+	public int isolateField(String company, int min, int max) {
+		int mid = (min + max) / 2;
+		if (tmpNames.get(mid).equals(company)) return mid;
+		else if (tmpNames.get(mid).compareTo(company) >0) {
+			return isolateField(company, min, mid-1);
+		} else {
+			return isolateField(company, mid+1, max);
+		}
+	}
+	
+	//Finds exactly how many instances of 
+	public int endingField(String company) {
+		int startPos = isolateField(company, 0, tmpNames.size());
+		int count = 0;
+		for (int i = startPos; i < tmpNames.size(); i++) {
+			if (tmpNames.get(i).equals(company)) count++;
+		}
+		return count;
+	}
+	
+	/** Constructor **/ 
 	public GraphViewer() {
 		
 		//Create reader object
 		ReadCVS reader = new ReadCVS();
 		
 		//Run main method so that we have all our data in the two Queues
+		//Sizes are equal
 		reader.run();
 		Queue<Float> probabilityQueue =  reader.probabilityQ();
 		Queue<String> nameQueue = reader.probablityN();
+		
+		Map<String, Float> toolMap = new HashMap<String, Float>();
+		
+//		for (int i = 0; i < probabilityQueue.size(); i++) {
+//			toolMap.put(nameQueue.remove()+":"+nameQueue.remove(), probabilityQueue.remove());
+//		}
+		
 		//Queue<String> tmpQueue = nameQueue;
 		
 		//Create new graph
 		g = new DirectedSparseMultigraph<String, String>();
 				
-		//Data structures to be utilized
-		ArrayList<String> tmpNames = new ArrayList<String>();
-		ArrayList<Float> prb = new ArrayList<Float>();
 		
-		//boolean done = false;
+		
 		while (nameQueue.size() != 0) {
-			//if (nameQueue.peek() == null) 
 			tmpNames.add(nameQueue.remove());
-			//System.out.println("here");
 		}
 		
 		while (probabilityQueue.size() != 0) {
@@ -62,78 +100,70 @@ public class GraphViewer {
 		}
 		
 		
-		//System.out.println("First test");
+		//Grab input from user
+		System.out.println("Enter a tech tool to graph: ");
+		String in = scanner.nextLine();
 		
-		//[][][][][][]Debugging[][][][][][][][]
-		//System.out.println(tmpNames.toString());
-
-		//System.out.println(prb.toString());
+		int startPos = isolateField(in, 0, tmpNames.size());
+		int nSize = endingField(in);
+		int endPos = startPos + nSize;
 		
-//		for (int i = 0; i < tmpNames.size(); i++) {
-//				//	If two tool names are matching strings, then delete the second data point
-//				// - and delete the probability value between the association.
-//			if (tmpNames.get(i).equals(i+1)) {
-//				tmpNames.remove(i);
-//				prb.remove(i);
-//			}
-//		}
-		
-//		tmpNames.remove(0);
-//		tmpNames.remove(0);
-//		prb.remove(0);
-//		System.out.println(tmpNames.toString());
-		
-		
-		
-		
-		//Create Vertices for each tool name
-		//[][][][] Issues present here [][][][][][]
-		//tmpNames.size()-1
-		for (int i = 0; i < 25; i++) {
+		//Add edges and vertices to the Graph object
+		for (int i = startPos; i < endPos; i++) {
 			int j = 0;
-			Random rnd = new Random();
+			Random rnd1 = new Random();
+			Random rnd2 = new Random();
 			//if (tmpNames.get(i+1) == null) break;
-			String tmpVal = j + "" + tmpNames.get(i).charAt(0) + tmpNames.get(i).charAt(tmpNames.get(i).length()-1) 
-					+ "," + tmpNames.get(i+1).charAt(0) + tmpNames.get(i+1).charAt(tmpNames.get(i+1).length()-1) + 
-					rnd.nextInt(1000000) + tmpNames.get(i+1).charAt(tmpNames.get(i+1).length()-2);
-			j++;
+			String tmpVal = tmpNames.get(i) + ":" + rnd1.nextInt(100000) + tmpNames.get(i+1)
+			+ rnd2.nextInt(100000) + rnd1.nextInt(100000);
+			
 			g.addVertex(tmpNames.get(i));
 			
 			if (g.findEdge(tmpNames.get(i), tmpNames.get(i+1)) != null) {
 				g.removeEdge(g.findEdge(tmpNames.get(i), tmpNames.get(i+1)));
 			} else {
-				g.addEdge("P: " + tmpVal + " -> "+ prb.get(i).toString(), tmpNames.get(i), tmpNames.get(i+1));
-				//g.addEdge(new MyEdge(prb.get(i)).toString(), tmpNames.get(i), tmpNames.get(i+1));
+				g.addEdge(tmpVal + "P: " + prb.get(i).toString(), tmpNames.get(i), tmpNames.get(i+1));
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		GraphViewer gv = new GraphViewer();
-		Layout<String, String> layout = new CircleLayout<String, String>(gv.g);
-		layout.setSize(new Dimension(1200, 1200));
-		BasicVisualizationServer<String, String> vv = new BasicVisualizationServer<String, String>(layout);
-		vv.setPreferredSize(new Dimension(1500, 1500));
-		
-		// show vertex and edge labels
-		vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller<String>());
-		vv.getRenderContext().setEdgeLabelTransformer(new ToStringLabeller<String>());
-		
-		//Create a graph mouse and add it to the visualization component
-		DefaultModalGraphMouse gm = new DefaultModalGraphMouse();
-		gm.setMode(ModalGraphMouse.Mode.TRANSFORMING);
-		//vv.set(gm);
-		
-		
-		
+		 	GraphViewer sgv = new GraphViewer(); // Creates the graph...
+	        // Layout<V, E>, VisualizationComponent<V,E>
+	        Layout<String, String> layout = new CircleLayout(sgv.g);
+	        layout.setSize(new Dimension(1000,1000));
+	        VisualizationViewer<String,String> vv = new VisualizationViewer<String,String>(layout);
+	        vv.setPreferredSize(new Dimension(1050,1050));
+	        // Show vertex and edge labels
+	        vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
+
+	        // Create a graph mouse and add it to the visualization component
+	        DefaultModalGraphMouse gm = new DefaultModalGraphMouse();
+	        gm.setMode(ModalGraphMouse.Mode.TRANSFORMING);
+	        vv.setGraphMouse(gm); 
+	        JFrame frame = new JFrame("Interactive Graph View 1");
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        frame.getContentPane().add(vv);
+	        frame.pack();
+	        frame.setVisible(true);
 		
 		//Setup a new vertex to paint transformer..
 		Transformer<String, Paint> vertexPaint = new Transformer<String, Paint>() {
 			public Paint transform(String i) {
-				return Color.GREEN;
+				return Color.BLUE;
 			}
 		};
 		
+        vv.getRenderContext().setVertexFillPaintTransformer(vertexPaint);
+        
+		
+		//Make edges more presentable
+        vv.getRenderContext().setEdgeLabelTransformer(new Transformer<String, String>() {
+        	public String transform(String c) {
+        		return StringUtils.substringAfterLast(c, ":");
+        	}
+        });
+        
 		//Setup a new stroke Transformer for edges
 		float dash[] = {10.0f};
 		final Stroke edgeStroke = new BasicStroke(1.0f, BasicStroke.CAP_BUTT,
@@ -145,18 +175,15 @@ public class GraphViewer {
 			}
 		};
 		
-		vv.getRenderContext().setVertexFillPaintTransformer(vertexPaint);
+		//vv.getRenderContext().setVertexFillPaintTransformer(vertexPaint);
         vv.getRenderContext().setEdgeStrokeTransformer(edgeStrokeTransformer);
-        vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
-        vv.getRenderContext().setEdgeLabelTransformer(new ToStringLabeller());
-        vv.getRenderer().getVertexLabelRenderer().setPosition(Position.CNTR);        
+        vv.getRenderer().getVertexLabelRenderer().setPosition(Position.CNTR); 
+        
+        //Distance Statistics
+        DijkstraShortestPath<String, String> dsp = new DijkstraShortestPath<String, String>(g);
+        Transformer<String, Double> distances = DistanceStatistics.averageDistances(g, dsp);
         
         
-        JFrame frame = new JFrame("Simple Graph View 2");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(vv);
-        frame.pack();
-        frame.setVisible(true);
         
 	}
 	
